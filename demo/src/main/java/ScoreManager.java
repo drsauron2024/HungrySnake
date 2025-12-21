5e14158b40a767f1bffbd8b4d83b4448f896bbd1
10245101440-wuyu <2862215@qq.com>
2025-12-21 16:00:51 +0800

    1

--- demo/src/main/java/ScoreManager.java
+++ demo/src/main/java/ScoreManager.java
@@ -1,3 +1,27 @@
 public class ScoreManager {
-    
+    private int score = 0;
+    private int combo = 0;
+    private int highestCombo = 0;
+
+    public void eatFood(FoodType type) {
+        combo++;
+        highestCombo = Math.max(highestCombo, combo);
+        score += type.scoreForCombo(combo);
+    }
+
+    public void resetCombo() {
+        combo = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getCombo() {
+        return combo;
+    }
+
+    public int getHighestCombo() {
+        return highestCombo;
+    }
 }
